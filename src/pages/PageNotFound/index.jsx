bf6cc998f48165e3716612c7f1fd79bf81b1c69a
oliver75 <oliver@m-power.io>
2020-09-13 22:59:27 +0300

    style 404 page

--- src/pages/PageNotFound/index.jsx
+++ src/pages/PageNotFound/index.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Alert from "react-bootstrap/Alert";
+import { Link } from "react-router-dom";
 
 const PageNotFoundPage = () => {
-  return <div>404</div>;
+  return (
+    <Alert variant="danger" className="page-not-found">
+      <Alert.Heading>Oopsies!</Alert.Heading>
+      <p>
+        You knocked on the wrong door. The page you are looking for does not
+        exist. Maybe you are looking for{" "}
+        <Link to={`/login`} className="text-info">
+          login
+        </Link>{" "}
+        or{" "}
+        <Link to={`/register`} className="text-info">
+          register
+        </Link>{" "}
+        or{" "}
+        <Link to={`/books`} className="text-info">
+          books
+        </Link>
+      </p>
+    </Alert>
+  );
 };
 
 export default PageNotFoundPage;
